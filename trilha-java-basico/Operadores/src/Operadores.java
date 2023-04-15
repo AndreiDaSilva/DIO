98cc60d49c15eb2217a9d4505e6980246abcacf0
Andrei Robson da Silva <andreirobson@furb.br>
2023-04-14 23:22:24 -0300

    incrementação

---
+++ trilha-java-basico/Operadores/src/Operadores.java
@@ -0,0 +1,33 @@
+public class Operadores {   
+
+    public static void main(String[] args) {
+        
+        
+        String nomeCompleto = "LINGUAGEM" + " " + "JAVA";
+        System.out.println(nomeCompleto);
+        
+        String concatenacao = "?";
+        
+        System.out.println(concatenacao);
+        concatenacao = 1 + 1 + 1 + "1";
+        System.out.println(concatenacao);
+        concatenacao = 1+"1"+1+1;
+        System.out.println(concatenacao);
+        concatenacao = 1+"1"+1+"1";
+        System.out.println(concatenacao);
+        concatenacao = "1"+1+1+1;
+        System.out.println(concatenacao);
+
+        //incrementação
+        int numero = 5;
+
+        numero++;
+
+        System.out.println(numero);
+        
+        
+    }
+
+
+    
+}
