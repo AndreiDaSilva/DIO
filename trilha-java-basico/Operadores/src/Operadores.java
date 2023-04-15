a16f94dcbfd32c6d7f63642b5cbab092280839f1
Andrei Robson da Silva <andreirobson@furb.br>
2023-04-14 23:31:13 -0300

    Ternário

--- trilha-java-basico/Operadores/src/Operadores.java
+++ trilha-java-basico/Operadores/src/Operadores.java
@@ -1,49 +1,65 @@
-public class Operadores {   
+public class Operadores {
 
     public static void main(String[] args) {
-        
-        
+
         String nomeCompleto = "LINGUAGEM" + " " + "JAVA";
         System.out.println(nomeCompleto);
-        
+
         String concatenacao = "?";
-        
+
         System.out.println(concatenacao);
         concatenacao = 1 + 1 + 1 + "1";
         System.out.println(concatenacao);
-        concatenacao = 1+"1"+1+1;
+        concatenacao = 1 + "1" + 1 + 1;
         System.out.println(concatenacao);
-        concatenacao = 1+"1"+1+"1";
+        concatenacao = 1 + "1" + 1 + "1";
         System.out.println(concatenacao);
-        concatenacao = "1"+1+1+1;
+        concatenacao = "1" + 1 + 1 + 1;
         System.out.println(concatenacao);
 
-        //incrementação
+        // incrementação
         int numero = 5;
 
         numero++;
 
         System.out.println(numero);
 
-        numero =+ 1;
+        numero = +1;
 
         System.out.println(numero);
 
         numero--;
 
         System.out.println(numero);
-        
-        //negação de boolean
+
+        // negação de boolean
 
         boolean ehVerdade = true;
-        
+
         System.out.println(!ehVerdade);
 
         ehVerdade = !ehVerdade;
 
         System.out.println(ehVerdade);
+
+        // Ternário
+
+        int a, b;
+
+        a = 5;
+        b = 6;
+        String resultado;
+
+        if (a==b) {
+            resultado = "verdadeiro";
+        } else {
+            resultado = "falso";
+        }
+        System.out.println(resultado);
+        
+        resultado = a == b ? "verdadeiro" : "falso";
+        //true o false;
+        System.out.println(resultado);
     }
 
-
-    
 }
