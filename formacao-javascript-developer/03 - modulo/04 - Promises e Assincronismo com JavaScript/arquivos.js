3a3ece4d927de94bf3ea633433f6d358920d8291
Andrei Robson <andreirobson@furb.br>
2023-09-26 21:26:41 -0300

    async e await

--- formacao-javascript-developer/03 - modulo/04 - Promises e Assincronismo com JavaScript/arquivos.js
+++ formacao-javascript-developer/03 - modulo/04 - Promises e Assincronismo com JavaScript/arquivos.js
@@ -2,22 +2,37 @@ const files = require('fs')
 const path = require('path')
 
 const filePath = path.resolve(__dirname, 'task.csv')
-const file = files.promises.readFile(filePath)
 
-file
-.then((arquivo) => arquivo.toString('utf-8'))
-.then((text) => text.split('\n').slice(1))
-.then((linha) => linha.map((linhas) => {
-    const [nome, feito] = linhas.split(";")
-    return {
-        nome, 
-        feito: feito === 'true'
+
+// file
+// .then((arquivo) => arquivo.toString('utf-8'))
+// .then((text) => text.split('\n').slice(1))
+// .then((linha) => linha.map((linhas) => {
+//     const [nome, feito] = linhas.split(";")
+//     return {
+//         nome, 
+//         feito: feito === 'true'
+//     }
+// }))
+// .then((listaTarefas) => console.log(listaTarefas)) 
+// .catch((error) => {
+//     console.log('Deu ruim', error)
+// })
+// .finally(() =>{
+//     console.log('Finalizou!')
+// })
+
+async function buscarArquivo() {
+    try {
+        const file = await files.promises.readFile(filePath);
+        const textFile = file.toString('utf-8')
+        console.log(textFile)
+    } catch (error) {
+        console.log(error)
+    } finally {
+        console.log('Finalizou!')
     }
-}))
-.then((listaTarefas) => console.log(listaTarefas)) 
-.catch((error) => {
-    console.log('Deu ruim', error)
-})
-.finally(() =>{
-    console.log('Finalizou!')
-})
+
+}
+
+buscarArquivo();
