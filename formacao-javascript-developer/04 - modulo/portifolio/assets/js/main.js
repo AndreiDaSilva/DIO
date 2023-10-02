6d2ddf53710d453621ea40f2ef1147a377406f3a
Andrei Robson <andreirobson@furb.br>
2023-10-01 22:56:33 -0300

    informações sincronizadas

--- formacao-javascript-developer/04 - modulo/portifolio/assets/js/main.js
+++ formacao-javascript-developer/04 - modulo/portifolio/assets/js/main.js
@@ -1,8 +1,31 @@
+function updateProfileInfo(profileData){
+    const photo = document.getElementById('profile.photo')
+    photo.src = profileData.photo
+    photo.alt = profileData.name
 
+    const name = document.getElementById('profile.name')
+    name.innerText = profileData.name
 
+    const person = document.getElementById('profile.person')
+    person.innerText = profileData.person
+
+    const local = document.getElementById('profile.local')
+    local.innerText = profileData.local
+
+    const phone = document.getElementById('profile.phone')
+    phone.innerText = profileData.phone
+    phone.href = `tel:${profileData.phone}`
+
+    const envelope = document.getElementById('profile.envelope')
+    envelope.innerText = profileData.envelope
+    envelope.href = `maito:${profileData.phone}`
+
+    
+
+}
 
 
 (async () => {
     const profileData = await fetchProfileData()
-    console.log(profileData)
+    updateProfileInfo(profileData)
 })()
