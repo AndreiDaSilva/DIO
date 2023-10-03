bde914a5bc90b4dbb2db72e3c9001e3be678cff8
Andrei Robson <andreirobson@furb.br>
2023-10-03 19:08:20 -0300

    profile.languages

--- formacao-javascript-developer/04 - modulo/portifolio/assets/js/main.js
+++ formacao-javascript-developer/04 - modulo/portifolio/assets/js/main.js
@@ -1,4 +1,4 @@
-function updateProfileInfo(profileData){
+function updateProfileInfo(profileData) {
     const photo = document.getElementById('profile.photo')
     photo.src = profileData.photo
     photo.alt = profileData.name
@@ -19,13 +19,29 @@ function updateProfileInfo(profileData){
     const envelope = document.getElementById('profile.envelope')
     envelope.innerText = profileData.envelope
     envelope.href = `maito:${profileData.phone}`
+}
 
-    
+function updateSoftSkills(profileData) {
+    const softSkills = document.getElementById('profile.skills.softSkills')
+    softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('')
 
 }
 
+function updateHardSkills(profileData) {
+    const hardSkills = document.getElementById('profile.skills.hardSkills')
+    hardSkills.innerHTML = profileData.skills.hardSkills.map(skill =>
+        `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`).join('')
+}
+
+function updateLanguages(profileData){
+    const languages = document.getElementById('profile.languages')
+    languages.innerHTML = profileData.languages.map(languages => `<li>${languages}</li>`).join('')
+
+}
 
 (async () => {
     const profileData = await fetchProfileData()
     updateProfileInfo(profileData)
+    updateSoftSkills(profileData)
+    updateHardSkills(profileData)
 })()
