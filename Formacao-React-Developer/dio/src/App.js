19047effe5d658ec474e85cc292588c00ff6bf25
Andrei Robson <andreirobson@furb.br>
2023-12-18 19:43:04 -0300

    primeira versão projeto da calculadora

--- Formacao-React-Developer/dio/src/App.js
+++ Formacao-React-Developer/dio/src/App.js
@@ -1,7 +1,11 @@
+import Button from './components/Button';
 
 function App() {
   return (
-    <div className="App"> Olá React </div>
+    <div className="App">
+      <h1>Olá React</h1>
+      <Button title="click aqui"/>
+    </div>
   );
 }
 
