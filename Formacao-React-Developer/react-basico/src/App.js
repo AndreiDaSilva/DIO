3ec2c693b91136ca14b7f6fd7b769ff1c95d9b9a
Andrei Robson <andreirobson@furb.br>
2023-12-19 21:27:12 -0300

    criação e configuração manual de um projeto react

--- Formacao-React-Developer/react-basico/src/App.js
+++ Formacao-React-Developer/react-basico/src/App.js
@@ -1,15 +1,38 @@
+import React, { useState, useEffect } from "react";
+
 const App = () => {
-    const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const [user, setUser] = useState("")
+    const [cont, setCount] = useState(0);
+    const [users, setUsers] = useState(["Pablo", "Andrei", "Tana"]);
+
+
+    const handleAddUser = () => {
+        if (users.includes(user)) {
+            setUser("já existente")
+        } else {
+            setUsers([...users, user])
+            setUser("")
+        }
+    }
+
+    useEffect(() => {
+        setCount(users.length)
+    }, [users])
+    
     return (
         <div className="App">
-            <h1>
-                Olá React
-            </h1>
-            {arr.map((item) => 
-            <p>
-                {item} * 5 = {item * 5}
-            </p>
-            )}
+            <h1> Olá React </h1>
+            <h3>Total: {cont}</h3>
+            <div>
+                <input
+                    value={user}
+                    onChange={(event) => setUser(event.target.value)} />
+                <button onClick={handleAddUser}>Adicionar</button>
+            </div>
+            {users.map((item) => (
+                <p>{item}</p>
+            ))}
+
         </div>
     )
 }
