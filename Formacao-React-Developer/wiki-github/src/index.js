dd198abce3a0c60801875bc8883bb0f4887356df
Andrei Robson da Silva <silva.andrei@outlook.com.br>
2023-12-26 15:38:05 +0000

    projeto corrigido / add chakra ui e react icons

--- Formacao-React-Developer/wiki-github/src/index.js
+++ Formacao-React-Developer/wiki-github/src/index.js
@@ -2,14 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './pages/App';
 
-import GlobalStyles from "./styles"
-import { ChakraProvider } from '@chakra-ui/react';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ChakraProvider>
-    <React.StrictMode>
-      <GlobalStyles />
-      <App />
-    </React.StrictMode>
-  </ChakraProvider>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
--- Formacao-React-Developer/wiki-github/src/pages/App.js
+++ Formacao-React-Developer/wiki-github/src/pages/App.js
@@ -1,7 +1,12 @@
+import { Icon } from "@chakra-ui/icon";
+import {FaGithub} from "react-icons/fa"
+
 function App() {
   return (
     <div className="App">
-      <h1>Olá Mundo</h1>
+      <Icon w={72} h={72}>
+        <FaGithub />
+      </Icon>
     </div>
   );
 }
