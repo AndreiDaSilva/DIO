12dcef28d5c47aa3f3c9e9f74e043925416d3ca3
Andrei Robson da Silva <silva.andrei@outlook.com.br>
2023-12-27 20:30:18 +0000

    props button ajustado

--- Formacao-React-Developer/wiki-github/src/Components/Button/index.js
+++ Formacao-React-Developer/wiki-github/src/Components/Button/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import * as C from '@chakra-ui/react'
 
-export default function Button(contend) {
+export default function Button({contend}) {
     return (
         <>
             <C.Button h='1.75rem' size='sm' bg={'green'}>
