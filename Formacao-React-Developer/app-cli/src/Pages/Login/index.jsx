2e5ea24d4c2d4dd871b75f83f624f813dca656fd
Andrei Robson <andreirobson@furb.br>
2024-01-23 19:55:15 -0300

    trabalhando com aixos e hook forms

--- Formacao-React-Developer/app-cli/src/Pages/Login/index.jsx
+++ Formacao-React-Developer/app-cli/src/Pages/Login/index.jsx
@@ -10,17 +10,54 @@ import {
   SubTitleLogin,
 } from "./styles";
 
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
 import { MdEmail, MdLock } from "react-icons/md";
 import { Header } from "../../components/Header";
 import { Button } from "../../components/Button";
 import { Input } from "../../components/Input";
 import { useNavigate } from "react-router-dom";
+import { api } from "../../services/api";
+
+const schema = yup
+  .object({
+    email: yup
+      .string()
+      .email("Email não é valido")
+      .required("Campo obrigatorio"),
+    password: yup
+      .string()
+      .min(8, "No minimo 8 caracteres")
+      .required("Campo obrigatorio"),
+  })
+  .required();
 
 export const Login = () => {
   const navigate = useNavigate();
 
-  const handleClickSignIn = () => {
-    navigate("/feed");
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+    mode: "onChange",
+  });
+
+  const onSubmit = async formData => {
+    try {
+      const { data } = await api.get(
+        `users?email=${formData.email}&senha=${formData.password}`
+      );
+      if (data.length === 1) {
+        navigate("/feed");
+      } else {
+        alert('Email ou senha invalido')
+      }
+    } catch (error) {
+      alert("Houve um erro, tente novamente.");
+    }
   };
 
   return (
@@ -37,14 +74,23 @@ export const Login = () => {
           <Wrapper>
             <TitleLogin>Faça seu cadastro</TitleLogin>
             <SubTitleLogin>Faça seu login e make the change.</SubTitleLogin>
-            <form action="">
-              <Input placeholder="email" leftIcon={<MdEmail />} />
+            <form onSubmit={handleSubmit(onSubmit)}>
               <Input
+                name="email"
+                errorMessage={errors?.email?.message}
+                control={control}
+                placeholder="email"
+                leftIcon={<MdEmail />}
+              />
+              <Input
+                name="password"
+                errorMessage={errors?.password?.message}
+                control={control}
                 placeholder="senha"
                 type="password"
                 leftIcon={<MdLock />}
               />
-              <Button title={"Entrar"} variant="secondary" onClick={handleClickSignIn}/>
+              <Button title={"Entrar"} variant="secondary" type="submit" />
             </form>
             <Row>
               <EsqueciText>Esqueci minha senha</EsqueciText>
--- Formacao-React-Developer/app-cli/src/Pages/Login/styles.js
+++ Formacao-React-Developer/app-cli/src/Pages/Login/styles.js
@@ -74,3 +74,4 @@ export const Row = styled.div`
     align-items: center;
     margin-top: 20px;
 `
+
