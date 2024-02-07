6f444788494e5c930ff10aa740e255bede71385f
andrei.silva <silva.andrei@outlook.com.br>
2024-02-07 19:21:39 -0300

    finalizado simulador de conta

--- Formacao-Java-Developer/desafio01/src/App.java
+++ Formacao-Java-Developer/desafio01/src/App.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        //TODO: Criar um apicação de simulação de conta de banco no terminal
+        Scanner tec = new Scanner(System.in);
+
+        ContaPessoa pessoa = new ContaPessoa();
+
+        System.out.println("Digite o numero da conta");
+        pessoa.setNumero(tec.nextInt());
+
+        System.out.println("Digite a agencia");
+        pessoa.setAgencia(tec.next());
+
+        System.out.println("Digite o nome completo");
+        pessoa.setNome(tec.next());
+
+        System.out.println("Digite o saldo da conta");
+        pessoa.setSaldo(tec.nextDouble());
+
+        System.out.println(pessoa.toString());
+
     }
 }
---
+++ Formacao-Java-Developer/desafio01/src/ContaPessoa.java
@@ -0,0 +1,72 @@
+public class ContaPessoa {
+    private Integer numero;
+    private String agencia;
+    private String nome;
+    private Double saldo;
+
+    /**
+     * @return the numero
+     */
+    public Integer getNumero() {
+        return numero;
+    }
+
+    /**
+     * @param numero the numero to set
+     */
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    /**
+     * @return the agencia
+     */
+    public String getAgencia() {
+        return agencia;
+    }
+
+    /**
+     * @param agencia the agencia to set
+     */
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the saldo
+     */
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    /**
+     * @param saldo the saldo to set
+     */
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+
+    @Override
+    public String toString() {
+        return "Olá " + nome +
+                ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +
+                ", conta " + numero +
+                " e seu saldo " + saldo +
+                " já está disponível para saque";
+    }
+
+}
