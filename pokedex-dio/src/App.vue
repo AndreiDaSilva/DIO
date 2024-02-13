254c0cd6fe3ccae836dd1b1a68379a447e4f4d8c
Andrei Robson <andreirobson@furb.br>
2024-02-13 20:25:13 -0300

    pokedex create project

---
+++ pokedex-dio/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+	<div id="root">
+		<h1>Olá</h1>
+	</div>
+</template>
+
+<script>
+
+export default {
+	name: 'App',
+
+};
+</script>
+
+<style lang="scss" scoped>
+#pokedex {
+	display: flex;
+	flex-direction: column;
+	min-height: 100vh;
+	max-width: 1000px;
+	margin: 0 auto;
+	padding: 2rem;
+}
+</style>
